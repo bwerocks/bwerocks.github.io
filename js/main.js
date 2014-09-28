65d2abd68b4a42e45ef57e26f9cb3ffae9d4dc10
William Martins <wwwhmartins@gmail.com>
2014-09-28 02:37:42 -0300

    Add target=_blank behavior

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+;(function() {
+    document.body.addEventListener && document.body.addEventListener('click',
+        function(e) {
+            if(e.target.tagName === 'A') {
+                e.preventDefault();
+                window.open(e.target.href);
+            }
+        }
+    );
+}());
