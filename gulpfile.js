3bfacab6ad4a7cdcc2ebabc46bce599aa8287f3e
William Martins <wwwhmartins@gmail.com>
2014-10-19 17:57:37 -0200

    Add scss structure; Also, add neat and bourbon

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+var paths = {
+    scss: './scss',
+    css: './css',
+    js: './js'
+};
+
+var files = {
+    scss: paths.scss + '/**/*.scss',
+    css: paths.css + '/**/*.css',
+    js: paths.js + '/**/*.js'
+};
+
+gulp.task('sass', function() {
+
+    gulp.src(files.scss).
+         pipe(sass({
+            includePaths: [paths.scss]
+         })).
+         pipe(gulp.dest(paths.css));
+
+});
+
+gulp.task('default', ['sass']);
+
+gulp.watch(files.scss, ['sass']);
